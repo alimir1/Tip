ddbac004262739cd7518998199e7d0788b800bdb
alimir1 <contact@alimir.io>
2017-08-29 17:05:23 -0700

    settings ui

---
+++ TipApp/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  TipApp
+//
+//  Created by Ali Mir on 8/29/17.
+//  Copyright © 2017 com.AliMir. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
